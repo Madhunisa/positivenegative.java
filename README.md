02df6be04d2c92f5f0c0874b2804f961c6a66402
Madhunisa <nisasenthil97@gmail.com>
2017-06-06 11:42:18 +0530

    Update and rename README.md to prog1.java

--- README.md
+++
@@ -1,1 +0,0 @@
-# positivenegative.java
---
+++ prog1.java
@@ -0,0 +1,22 @@
+# positivenegative.java
+import java.io.*;
+import java.util.*l;
+public class prog1
+{
+public static void main(String args[])
+{
+int s;
+if(s>0)
+{
+Syste.out.println(" positive");
+}
+else if(s<0)
+{
+System.out.println(" negative");
+}
+else
+{
+System.out.println("zero");
+}
+}
+}
